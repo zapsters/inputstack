85d3577e9da7173b25b612ab81705afacfcb3808
Zapster <findingslimes@gmail.com>
2023-09-05 18:40:25 -0400

    User sign in done?

--- firebase.js
+++ firebase.js
@@ -23,3 +23,53 @@ var token = null;
 function signInButton() {
   firebase.auth().signInWithRedirect(provider);
 }
+
+function onUserSignIn() {
+  if (user == null) return;
+  auth_signIn.style.display = "none";
+  auth_signedInAs.style.display = "block";
+  auth_signedInAs_displayName.innerHTML = user.displayName;
+}
+function onUserSignOut() {}
+
+//This gets the data after the redirect to google's log in page.
+firebase
+  .auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      token = credential.accessToken;
+    }
+    // The signed-in user info.
+    user = result.user;
+    // IdP data available in result.additionalUserInfo.profile.
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+// This subscribes to see if firebase gets an Authentication update, if so, log in as that user.
+// This recieves updates to check if the user is already logged in.
+firebase.auth().onAuthStateChanged((userData) => {
+  if (userData) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/v8/firebase.User
+    var uid = userData.uid;
+    user = userData;
+    onUserSignIn();
+    // ...
+  } else {
+    onUserSignOut();
+  }
+});
