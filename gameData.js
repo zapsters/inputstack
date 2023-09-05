598825140996c5e511755c0cab46d770e5da4a50
Zapster <findingslimes@gmail.com>
2023-09-05 10:48:35 -0400

    Version 1.00 - Testing 0.92

--- gameData.js
+++ gameData.js
@@ -21,7 +21,7 @@ var timer = document.getElementById("game_gameTimer_text");
 var versionText = document.getElementById("versionDiv");
 
 // Game Vars
-var version = "Release 1.00 - TESTING 0.91";
+var version = "Release 1.00 - TESTING 0.92";
 var host = false;
 var health = 100;
 var timerActive = false;
