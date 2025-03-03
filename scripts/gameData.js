77275d06351e9fd1e05e385a80fcb8424a5ebb70
Erin Ballinger <erballin@iu.edu>
2025-03-03 16:16:13 -0500

    1.3.0

--- scripts/gameData.js
+++ scripts/gameData.js
@@ -22,7 +22,7 @@ var timer = document.getElementById("game_gameTimer_text");
 var versionText = document.getElementById("versionDiv");
 
 // Game Vars
-var version = "1.2.1 Big Style Update!";
+var version = "1.3.0 Big Style Update!";
 var host = false;
 var defaultHealth = 100;
 var health = defaultHealth;
